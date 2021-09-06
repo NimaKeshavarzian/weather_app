8104bb9656e409590c9041e53edd7b0825337777
NimaKeshavarzian <nimakeshavarzian@gmail.com>
2021-09-06 22:20:11 +0430

    Start
    Create basic files and install packages

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <page-header :inputLocation="api.inputLocation" @location="api.inputLocation = $event"></page-header>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  data() {
+    return {
+      api: {
+        token: "18e14dd0589a07129fdf66b07af90ac8",
+        inputLocation: "tehran",
+        units: "metric",
+        url: `https://api.openweathermap.org/data/2.5/weather`
+      },
+      output: {
+        main: {
+          temp: null,
+          feelsLike: null,
+          min: null,
+          max: null,
+          humidity: null
+        },
+        weatherDetails: {
+          Desc: null,
+          icon: null
+        },
+        windSpeed: null,
+        location: {
+          country: null,
+          city: null
+        }
+      }
+    };
+  },
+  components: {
+    pageHeader: Header
+  }
+};
+</script>
+
+<style>
+/* Fonts */
+@import url("https://fonts.googleapis.com/css?family=Overpass");
+
+/* colors */
+:root {
+  --primaryColor: #fff;
+  --bgColor: linear-gradient(192.05deg, #47BFDF 0%, #4A91FF 100%);
+}
+* {
+  font-family: 'Overpass';
+}
+
+body {
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  box-sizing: border-box;
+  background: var(--bgColor);
+  color: var(--primaryColor);
+  overflow: hidden;
+}
+#app {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+  -ms-flex-flow: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+// App
+import Vue from 'vue'
+import App from './App.vue'
+
+// Render
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
